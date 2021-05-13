ab7c409760283b20466fde58f4148f01703c2b1c
Rachel Hendricks <rachelhen92@gmail.com>
2021-05-13 15:17:44 -0600

    Add componentDidMount and conditional rendering

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,19 +1,30 @@
 import React, { Component } from 'react';
 import Movies from '../Movies/Movies';
-import movieData from '../../movieData';
+import { getMovies } from '../../api-calls';
 
 class App extends Component {
   constructor() {
     super();
     this.state = {
-      movies: movieData.movies
+      movies: [],
+      error: ''
     }
+  }
+
+  componentDidMount() {
+    getMovies()
+      .then(data => {
+        this.setState({ movies: data.movies })
+      })
+      .catch(() => this.setState({ error: 'Something went wrong'}))
   }
 
   render() {
     return (
       <main className='App'>
         <h1>Rancid Tomatillos</h1>
+        {!this.state.movies.length && !this.state.error && <h2>Loading movies...</h2>}
+        {this.state.error && <h2>{this.state.error}</h2>}
         <Movies movies={this.state.movies} />
       </main>
     )
