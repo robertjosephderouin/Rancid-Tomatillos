e4d56defbfaa57830fc0c25dd65cc831996ff120
Rachel Hendricks <rachelhen92@gmail.com>
2021-05-18 15:18:40 -0600

    Add Home component

---
+++ src/components/Home/Home.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './Home.css';
+
+const Home = () => {
+  return(
+    <section>
+      <h1 className='title'>🍅Rancid</h1>
+      <h1 className='title'>Tomatillos🍅</h1>
+    </section>
+  )
+}
+
+export default Home;
