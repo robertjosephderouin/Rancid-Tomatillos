cb621814e07bf2ead51454568dba8db9ec5501ef
robertjosephderouin <robertjosephderouin@gmail.com>
2021-05-21 16:15:06 -0700

    Add overview data with seperate fetch

--- src/components/Spotlight/Spotlight.js
+++ src/components/Spotlight/Spotlight.js
@@ -1,20 +1,41 @@
 import React from 'react';
 import './Spotlight.css';
-import { Link, Redirect } from 'react-router-dom';
+import { getOverview } from '../../api-calls';
+import { Link } from 'react-router-dom';
 
-const Spotlight = ({ id, poster_path, backdrop_path, title, average_rating, release_date }) => {
-  return (
-    <div className='spotlight-container'>
-    {!release_date && <Redirect to='/' />}
-    <article className='spotlight-card' key={id}>
-      <img className='spotlight-image' src={backdrop_path} alt={title}/>
-      <h2>{title}</h2>
-      <p>🍅 {Math.round(average_rating)}</p>
-      <p>Released {new Date(release_date).toDateString()}</p>
-      <Link to={'/'} className='back-button'>🔙</Link>
-    </article>
-    </div>
-  )
+class Spotlight extends React.Component {
+  constructor({id}) {
+    super();
+    this.state = {
+      id: id,
+      spotLight: null,
+      error: '',
+    }
+  }
+
+  componentDidMount = () => {
+    getOverview(this.state.id)
+      .then(data => {
+        this.setState({ spotLight: data.movie })
+      })
+      .catch(() => this.setState({ error: 'Something went wrong'}))
+  }
+
+  render() {
+    return (
+      <div className='spotlight-container'>
+        <article className='spotlight-card' key={this.state.spotLight?.id}>
+          <img className='spotlight-image' src={this.state.spotLight?.backdrop_path} alt={this.state.spotLight?.title}/>
+          <h2>{this.state.spotLight?.title}</h2>
+          <p>🍅 {Math.round(this.state.spotLight?.average_rating)}</p>
+          <p>{this.state.spotLight?.overview}</p>
+          <p>Released {new Date(this.state.spotLight?.release_date).toDateString()}</p>
+          <Link to={'/'} className='back-button'>🔙</Link>
+        </article>
+      </div>
+    )
+  }
 }
+
 
 export default Spotlight;
